b1f6a7bd836ca9944a03f9d3cd52b7de750c080c
karthikraja46 <96817647+karthikraja46@users.noreply.github.com>
2025-05-16 19:43:49 +0530

    learned basics of dictionary

--- corey_schafer/dictionary.py
+++ corey_schafer/dictionary.py
@@ -1,1 +1,13 @@
 student = {'name': 'John', 'age': 25, 'courses': ['math', 'cs']}
+# student['phone'] = '555-5555'
+# student['name'] = 'Jane'
+# print(student.get('name',''))
+
+# student.update({'name': 'Jane', 'age': 26, 'phone': '555-5555'})
+# print(student)
+
+# dictionary comprehension
+
+for key,value in student.items():
+    print(key, value)
+ 
