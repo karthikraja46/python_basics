0fa233637a50878594c45db46cbc5acd02c705dd
karthikraja46 <96817647+karthikraja46@users.noreply.github.com>
2025-08-07 20:03:10 +0530

    solved

---
+++ dsa_new/remove_duplicates_from_array.py
@@ -0,0 +1,14 @@
+# Input: nums = [1,1,2]
+# Output: 2, nums = [1,2,_]
+
+
+def removeDuplicates(nums):
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+        return i+1
+
+nums = [1,1,2]
+print('remove duplicates from array',removeDuplicates(nums))
