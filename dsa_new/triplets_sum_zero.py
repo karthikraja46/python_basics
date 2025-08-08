d428df2df860c3e6e65924570136f194c696c58d
karthikraja46 <96817647+karthikraja46@users.noreply.github.com>
2025-08-08 16:04:43 +0530

    solved

---
+++ dsa_new/triplets_sum_zero.py
@@ -0,0 +1,23 @@
+
+def findTriplets(arr):
+    
+    map = {}
+    ans = []
+    for j in range(len(arr)):
+        for k in range(j + 1, len(arr)):
+            val = -1 * (arr[j] + arr[k])
+            if val in map:
+                for i in map[val]:
+                    ans.append([i, j, k])
+        
+        if arr[j] not in map:
+            map[arr[j]] = []
+        map[arr[j]].append(j)
+    
+    return ans
+
+if __name__ == "__main__":
+    arr = [0, -1, 2, -3, 1]
+    res = findTriplets(arr)
+    for triplet in res:
+        print(triplet[0], triplet[1], triplet[2])
