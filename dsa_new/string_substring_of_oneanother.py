758a78711e8bba7f134f552e0baee9290dbedfc0
karthikraja46 <96817647+karthikraja46@users.noreply.github.com>
2025-08-17 16:13:27 +0530

    solved

---
+++ dsa_new/string_substring_of_oneanother.py
@@ -0,0 +1,7 @@
+s1 = "hello world"
+s2 = "world"
+
+if s2 in s1:
+    print("Yes, s2 is a substring of s1")
+else:
+    print("No, s2 is not a substring of s1")
---
+++ dsa_new/valid_anagram.py
@@ -0,0 +1,32 @@
+# # Brute Force
+# # def valid_anagram(s:str, t:str) -> bool:
+# #     if len(s) != len(t):
+# #         return False
+# #     else:
+# #         return sorted(s) == sorted(t)
+    
+# # s = 'anagram'
+# # t = 'nagaram'
+# # print('the two strings are anagram to each other',valid_anagram(s,t))
+
+# # Optimal solution
+
+def valid_anagram(s:str, t:str) -> bool:
+    if len(s) != len(t):
+        return False
+    
+    char_count = {}
+    for char in s:
+        char_count[char] = char_count.get(char, 0)+1
+        
+    for char in t:
+        if char not in char_count or char_count[char] == 0:
+            return False
+        
+        char_count[char] -= 1
+
+    return True
+
+s = 'anagram'
+t = 'car'
+print('the two strings are valid anagrams', valid_anagram(s,t))
