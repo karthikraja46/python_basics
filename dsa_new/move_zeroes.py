ac5080a0dd63a2af14f94db6b048a338bee49104
karthikraja46 <96817647+karthikraja46@users.noreply.github.com>
2025-08-18 14:24:32 +0530

    made into a function returned nums

--- dsa_new/move_zeroes.py
+++ dsa_new/move_zeroes.py
@@ -6,13 +6,25 @@
 # Input: nums = [0]
 # Output: [0]
 
-nums = [0,1,0,3,12]
-n = len(nums)
+# nums = [0,1,0,3,12]
+# n = len(nums)
 
-left = 0
-for right in range(n):
-    if nums[right] != 0:
-        nums[left], nums[right] = nums[right], nums[left]
-        left += 1
+# left = 0
+# for right in range(n):
+#     if nums[right] != 0:
+#         nums[left], nums[right] = nums[right], nums[left]
+#         left += 1
 
-print('moved zeroes to the end',nums)
+# print('moved zeroes to the end',nums)
+
+def move_zeroes(nums):
+    n = len(nums)
+    left = 0
+    for right in range(n):
+        if nums[right] != 0:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+    return nums
+
+nums = [5,2,5,0,0,0,5,5,7,8]
+print('move the zeroes to end', move_zeroes(nums))
