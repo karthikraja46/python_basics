20d193417fc4b96c8248b7c94271c515b704eb39
karthikraja46 <96817647+karthikraja46@users.noreply.github.com>
2025-08-23 09:19:28 +0530

    solved this problem without any extra space

--- dsa_new/sum_of_distinct_elements.py
+++ dsa_new/sum_of_distinct_elements.py
@@ -1,12 +1,27 @@
-def sum_of_distinct_elements(nums):
-    map = {}
-    count = 0
-    for num in nums:
-        if num not in map:
-            count += num
-            map[num] = True
+# def sum_of_distinct_elements(nums):
+#     map = {}
+#     count = 0
+#     for num in nums:
+#         if num not in map:
+#             count += num
+#             map[num] = True
 
-    return count
+#     return count
+
+# nums = [1, 2, 3, 2, 4, 1]
+# print('sum of distinct elements',sum_of_distinct_elements(nums))
+
+
+# solving without any extra space
+
+def sum_of_distinct_elements(nums):
+    nums.sort()
+    total = nums[0]
+
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i-1]:
+            total += nums[i]
+    return total
 
 nums = [1, 2, 3, 2, 4, 1]
-print(sum_of_distinct_elements(nums))
+print('sum of distinct elements',sum_of_distinct_elements(nums))
