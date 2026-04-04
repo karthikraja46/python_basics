8506d07dcecd3ac680c04e8e1f47204d54ee05f6
karthikraja46 <96817647+karthikraja46@users.noreply.github.com>
2026-04-04 16:33:19 +0530

    used map inorder to reduce the complexity

--- dsa_new/count_distinct_elements.py
+++ dsa_new/count_distinct_elements.py
@@ -11,4 +11,16 @@ nums = [1, 2, 1, 3, 4, 2, 3]
 count = count_distinct_elements(nums)
 print('the count', count)
 
+#using the hashmap to count the distinct elements in the array
+def count_distinct_elements_optimal(nums):
+    map = {}
+    count = 0
+    for num in nums:
+        if num not in map:
+            count += 1
+            map[num] = True
+    return count
 
+nums = [1, 2, 1, 3, 4, 2, 3]
+count = count_distinct_elements_optimal(nums)
+print('the count', count)
